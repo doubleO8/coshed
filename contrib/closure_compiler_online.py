9956d5a1834b97516bde996d0c92e7dc2212c960
doubleO8 <wb008@hdm-stuttgart.de>
2017-11-03 22:25:49 +0100

    add a simple Closure Compiler Service API client script

---
+++ contrib/closure_compiler_online.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import requests
+
+# https://developers.google.com/closure/compiler/docs/api-tutorial1
+
+with open(sys.argv[1], 'rb') as src:
+    js_code = src.read()
+
+data = {
+    "output_format": "text",
+    "compilation_level": "SIMPLE_OPTIMIZATIONS",
+    "output_info": "compiled_code",
+    "js_code": js_code
+}
+
+req = requests.post('https://closure-compiler.appspot.com/compile', data=data)
+print req.text
