3d43ad8f15681d525e9f8cc3dc4cdde922b9fcf7
doubleO8 <wb008@hdm-stuttgart.de>
2020-02-10 09:54:26 +0100

    log settings

--- bundy.py
+++ bundy.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 from coshed import bundy
 
